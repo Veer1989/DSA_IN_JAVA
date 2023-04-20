983650e1b6c306e720181ff989fffc73add62b6b
veer <sjaiveer024@gmail.com>
2023-04-20 08:43:27 +0530

    INITIAL COMMIT

--- src/Conditionals/Largest_In_Three_Numbers.java
+++ src/Conditionals/Largest_In_Three_Numbers.java
@@ -1,5 +1,23 @@
+
 package Conditionals;
-
+import java.util.Scanner;
 public class Largest_In_Three_Numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b= sc.nextInt();
+        int c = sc.nextInt();
 
+        int max = 0 ;
+        if (a>b){
+            max = a ;
+        }else{
+            max= b ;
+        }
+
+        if(c>max){
+            max = c ;
+        }
+        System.out.println(max);
+    }
 }
